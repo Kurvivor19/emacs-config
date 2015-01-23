9d98be8f08bd3d1c8ee3c4570b9cd4f08c8f4e7f
Ivan Truskov <i.truskov@visionlabs.ru>
2015-01-23 17:01:22 +0300

    Added gnus config added machine-specific config for future reference

--- config/packages-preload.el
+++ config/packages-preload.el
@@ -1,4 +1,4 @@
-;; preload packages that are likely to be used all the time
+;;; preload packages that are likely to be used all the time
 
 ;; remember file places between sessions
 (require 'saveplace)
@@ -42,5 +42,8 @@
 ;; ensure ahg is loaded
 (eval-after-load "vc-hg"
   '(require 'ahg))
+
+;; change gnus configuration file location to keep it inside repository dir
+(setq gnus-init-file (concat dotfiles-dir ".gnus.el"))
 
 (provide 'packages-preload)
