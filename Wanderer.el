9a38ce841083c61d68467f2ae598efedbe9a5267
Ivan Truskov <trus19@gmail.com>
2017-02-19 22:35:52 +0300

    Added emms support to notebook

--- Wanderer.el
+++ Wanderer.el
@@ -6,8 +6,43 @@
 
 
 ;; putty paths
-(setenv "PATH" (concat (getenv "PATH") ":/cygdrive/c/Program\\ Files\\ (x86)/PuTTY"))
+(setenv "PATH" (concat (getenv "PATH") ":/cygdrive/c/Program Files (x86)/PuTTY"))
 (setq exec-path (append exec-path '("/cygdrive/c/Program Files (x86)/PuTTY")))
 
 ;; cmake support
 (require 'cmake-mode)
+
+;; emms support
+;; path to vlc
+(setenv "PATH" (concat (getenv "PATH") ":/cygdrive/c/Program Files (x86)/VideoLAN/VLC"))
+(setq exec-path (append exec-path '("/cygdrive/c/Program Files (x86)/VideoLAN/VLC")))
+
+;; allow using mediainfo
+(require 'emms-setup)
+(emms-all)
+(emms-default-players)
+(require 'emms-player-vlc)
+(require 'emms-info-mediainfo)
+(add-to-list 'emms-info-functions 'emms-info-mediainfo)
+
+;; Personal short keybindings
+(define-prefix-command 'user-map-f6)
+(global-set-key (kbd "<f6>") 'user-map-f6)
+;; e for emms
+(define-key user-map-f6 (kbd "e") 'emms)
+;; b for browser
+(define-key user-map-f6 (kbd "b") 'emms-smart-browse)
+;; r for random
+(define-key user-map-f6 (kbd "r") 'emms-random)
+;; s for stop
+(define-key user-map-f6 (kbd "s") 'emms-stop)
+;; n for next
+(define-key user-map-f6 (kbd "n") 'emms-next)
+;; p for previous
+(define-key user-map-f6 (kbd "p") 'emms-previous)
+;; P for Pause
+(define-key user-map-f6 (kbd "P") 'emms-pause)
+;; d for display
+(define-key user-map-f6 (kbd "d") 'emms-show)
+;; u for shUffle
+(define-key user-map-f6 (kbd "u") 'emms-shuffle)
