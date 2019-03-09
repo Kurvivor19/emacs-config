ea1d471ae5bf55fc5654010d5465f9348f321607
Ivan Truskov <trus19@gmail.com>
2019-03-09 23:51:39 +0300

    theme change

--- Wanderer.el
+++ Wanderer.el
@@ -1,13 +1,14 @@
 
 ;; make spaces visible
 (add-hook 'python-mode-hook
-	  (lambda ()
+    (lambda ()
             (whitespace-mode)))
 
 
 ;; putty paths
 (setenv "PATH" (concat (getenv "PATH") ":/cygdrive/c/Program Files (x86)/PuTTY"))
 (setq exec-path (append exec-path '("/cygdrive/c/Program Files (x86)/PuTTY")))
+
 
 ;; cmake support
 (require 'cmake-mode)
